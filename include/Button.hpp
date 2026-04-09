056bf9b31896903d37000905cb554e3c514175e1
limbo590052 <harry13142@gmail.com>
2026-04-09 23:07:46 +0800

    Add: Button

--- include/Button.hpp
+++ include/Button.hpp
@@ -1,14 +1,56 @@
-//
-// Created by limbo on 2026/3/27.
-//
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
 
-#ifndef REPLACE_WITH_YOUR_PROJECT_NAME_BUTTON_HPP
-#define REPLACE_WITH_YOUR_PROJECT_NAME_BUTTON_HPP
+#include <string>
+#include <functional>
+#include <memory>
 
+#include "Util/GameObject.hpp"
+#include "Util/Image.hpp"
 
-class Button {
+class Button : public Util::GameObject {
+public:
+    // 狀態定義
+    enum class State {
+        NORMAL,
+        HOVER,
+        CLICK
+    };
 
+    /**
+     * 建構子：傳入三種狀態的圖片路徑
+     */
+    Button(const std::string& name, const std::string& normal,
+           const std::string& hover, const std::string& click);
+
+    void Update();
+
+    // 事件設定
+    void SetOnClick(std::function<void()> callback) { m_OnClick = std::move(callback); }
+    void SetOnHover(std::function<void()> callback) { m_OnHover = std::move(callback); }
+    void SetOnLeave(std::function<void()> callback) { m_OnLeave = std::move(callback); }
+
+private:
+    /**
+     * 檢查滑鼠是否在按鈕矩形範圍內
+     */
+    bool IsMouseHovering() const;
+
+private:
+    std::string m_Name;
+    // 圖片資源
+    std::shared_ptr<Util::Image> m_NormalDrawable;
+    std::shared_ptr<Util::Image> m_HoverDrawable;
+    std::shared_ptr<Util::Image> m_ClickDrawable;
+
+    // 狀態紀錄
+    State m_CurrentState = State::NORMAL;
+    bool m_IsHoveringLastFrame = false;
+
+    // 回呼函式 (Callbacks)
+    std::function<void()> m_OnClick;
+    std::function<void()> m_OnHover;
+    std::function<void()> m_OnLeave;
 };
 
-
-#endif //REPLACE_WITH_YOUR_PROJECT_NAME_BUTTON_HPP
+#endif // BUTTON_HPP
--- src/Button.cpp
+++ src/Button.cpp
@@ -1,5 +1,69 @@
-//
-// Created by limbo on 2026/3/27.
-//
-
 #include "Button.hpp"
+#include "Util/Input.hpp"
+#include "Util/Logger.hpp" // 必須引入這個才能用 LOG_DEBUG
+
+Button::Button(const std::string& name, const std::string& normalPath,
+               const std::string& hoverPath, const std::string& clickPath): m_Name(name)
+        {
+    m_NormalDrawable = std::make_shared<Util::Image>(normalPath);
+    m_HoverDrawable = std::make_shared<Util::Image>(hoverPath);
+    m_ClickDrawable = std::make_shared<Util::Image>(clickPath);
+
+    m_Drawable = m_NormalDrawable;
+    m_ZIndex = 10.0f;
+}
+
+void Button::Update() {
+    if (!m_Visible) return;
+
+    bool isHoveringNow = IsMouseHovering();
+
+    // 紀錄：偵測滑鼠進入/離開
+    if (isHoveringNow && !m_IsHoveringLastFrame) {
+        LOG_DEBUG("[{}] Button '{}': Mouse Enter", __func__, m_Name);
+        if (m_OnHover) m_OnHover();
+    } else if (!isHoveringNow && m_IsHoveringLastFrame) {
+        LOG_DEBUG("[{}] Button '{}': Mouse Leave", __func__, m_Name);
+        if (m_OnLeave) m_OnLeave();
+    }
+
+    // 2. 狀態機邏輯
+    if (isHoveringNow) {
+        if (Util::Input::IsKeyDown(Util::Keycode::MOUSE_LB)) {
+            LOG_DEBUG("[{}] Button '{}': Mouse LB Down", __func__, m_Name);
+            m_CurrentState = State::CLICK;
+            m_Drawable = m_ClickDrawable;
+        } else {
+            // 偵測點擊釋放
+            if (m_CurrentState == State::CLICK) {
+                LOG_INFO("[{}] Button '{}': Click Triggered!", __func__, m_Name);
+                if (m_OnClick) m_OnClick();
+            }
+            m_CurrentState = State::HOVER;
+            m_Drawable = m_HoverDrawable;
+        }
+    } else {
+        m_CurrentState = State::NORMAL;
+        m_Drawable = m_NormalDrawable;
+    }
+
+    m_IsHoveringLastFrame = isHoveringNow;
+}
+
+bool Button::IsMouseHovering() const {
+    const auto mousePos = Util::Input::GetCursorPosition();
+    const auto size = m_Drawable->GetSize() * m_Transform.scale;
+    const auto halfSize = size / 2.0f;
+    const auto center = m_Pivot;;
+
+    // 計算邊界
+    float left = center.x - halfSize.x;
+    float right = center.x + halfSize.x;
+    float bottom = center.y - halfSize.y;
+    float top = center.y + halfSize.y;
+
+    bool hovering = mousePos.x >= left && mousePos.x <= right &&
+                    mousePos.y >= bottom && mousePos.y <= top;
+
+    return hovering;
+}
