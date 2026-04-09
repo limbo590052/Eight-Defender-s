928daaf285069f3205a2ce7d90bb3467380682a5
limbo590052 <harry13142@gmail.com>
2026-04-09 23:15:46 +0800

    Add  Background, StatrB to App

--- include/App.hpp
+++ include/App.hpp
@@ -2,6 +2,14 @@
 #define APP_HPP
 
 #include "pch.hpp" // IWYU pragma: export
+#include "PictureObj.hpp"
+#include "Button.hpp"
+#include "GameUnit.hpp"
+#include "Position.hpp"
+#include "Input.hpp"
+
+#define MAKE_BUTTON(var, normal, hover, click) \
+    var = std::make_shared<Button>(#var, normal, hover, click)
 
 class App {
 public:
@@ -24,6 +32,17 @@ private:
 
 private:
     State m_CurrentState = State::START;
+
+    // 剛才報錯缺少的變數都在這裡定義
+    std::shared_ptr<PictureObj> m_Background;
+    std::shared_ptr<Button> m_StartButton;
+    std::shared_ptr<PictureObj> m_startText;
+
+    int m_BaseHp = 100;
+    int m_Gold = 0;
+
+    std::vector<std::shared_ptr<GameUnit>> m_Enemies;
+    std::vector<glm::vec2> Waypoints = { {0,0}, {100,0} }; // 路徑點
 };
 
 #endif
